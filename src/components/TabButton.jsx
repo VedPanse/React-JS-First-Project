7a7b9a2f576d834276b6b9ce7cd9af60672e4233
Ved Panse <75316092+VedPanse@users.noreply.github.com>
2024-06-22 04:54:34 -0700

    Add files via upload

---
+++ src/components/TabButton.jsx
@@ -0,0 +1,5 @@
+export default function TabButton({children, onSelect, isSelected}) {
+    return <li>
+        <button onClick={onSelect} className={isSelected ? 'active' : undefined}>{children}</button>
+    </li>
+}
